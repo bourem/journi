5e6493da5a8ce886ca9fba07f7e6d652927839d1
RB <remi.bouchez@avep.io>
2017-07-23 17:48:00 +0900

    1. Add "id", integer primary key with autoincrement, to the db_init.py 2. Use executemany in db_populate_dummy.py, ta insert a couple dummy data at once

--- src/db_init.py
+++ src/db_init.py
@@ -5,7 +5,10 @@ if __name__ == "__main__":
     conn = sqlite3.connect('journi.db')
     c = conn.cursor()
     c.execute('''CREATE TABLE entries
-                 (date text, content text)''')
+                 (
+                 id integer primary key autoincrement,
+                 date text not null, 
+                 content text)''')
     conn.commit()
     conn.close()
 
--- src/db_populate_dummy.py
+++ src/db_populate_dummy.py
@@ -4,8 +4,13 @@ import sqlite3
 if __name__ == "__main__":
     conn = sqlite3.connect('journi.db')
     c = conn.cursor()
-    c.execute('''INSERT INTO entries
-                 VALUES ('2017-07-02', 'blabliblubleblo')''')
+    dummy_values = [
+            ("2017-01-01", "auienrstiuenrstaiunrset"),
+            ("2017-01-02", "bbbb b bb b bbbb bbbbb")
+            ]
+    c.executemany('''INSERT INTO entries(date, content)
+                 VALUES (?, ?)''',
+                 dummy_values)
     conn.commit()
     conn.close()
 
